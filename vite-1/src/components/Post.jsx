264ab158e65beab5abdb588456c5ee27ea3944fe
phiniqq724 <fahrellnew@gmail.com>
2024-02-03 08:43:35 +0700

    Pikek commit

--- vite-1/src/components/Post.jsx
+++ vite-1/src/components/Post.jsx
@@ -4,10 +4,10 @@ import classes from './Post.module.css';
 function Post(props) {
     return (
         <div className={classes.post}>
-            <p>{props.author}</p>
+            <p className="text-3xl">{props.author}</p>
             <p>{props.body}</p>
         </div>
-    ) 
+    );
 }
 
 export default Post;
